8655a35411f9c61efe7115e1eb5141aa73e54596
Osuolale <Osuolale@Macs-MacBook-Pro.local>
2024-03-06 09:08:13 +0100

    Readded expirationDate

---
+++ database/migrations/2024_03_06_072921_add_expiration_date_to_programmes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('programmes', function (Blueprint $table) {
+            //
+            Schema::table('programmes', function (Blueprint $table) {
+                // Drop the column if it exists
+                $table->dropColumn('expirationDate');
+            });
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('programmes', function (Blueprint $table) {
+            //
+            Schema::table('programmes', function (Blueprint $table) {
+                // Drop the column if it exists
+                $table->dropColumn('expirationDate');
+            });
+        });
+    }
+};
