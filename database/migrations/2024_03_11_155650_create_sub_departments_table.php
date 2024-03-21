fae6a7c3e9e1956c6fd5950af6d1d2e3d8fb4c6f
Osuolale <Osuolale@Macs-MacBook-Pro.local>
2024-03-21 13:21:14 +0100

    Commented out two migration files

--- database/migrations/2024_03_11_155650_create_sub_departments_table.php
+++ database/migrations/2024_03_11_155650_create_sub_departments_table.php
@@ -1,28 +1,28 @@
-<?php
+<?php 
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+// use Illuminate\Database\Migrations\Migration;
+// use Illuminate\Database\Schema\Blueprint;
+// use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('sub_departments', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->timestamps();
-        });
-    }
+// return new class extends Migration
+// {
+//     /**
+//      * Run the migrations.
+//      */
+//     public function up(): void
+//     {
+//         Schema::create('sub_departments', function (Blueprint $table) {
+//             $table->id();
+//             $table->string('name');
+//             $table->timestamps();
+//         });
+//     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('sub_departments');
-    }
-};
+//     /**
+//      * Reverse the migrations.
+//      */
+//     public function down(): void
+//     {
+//         Schema::dropIfExists('sub_departments');
+//     }
+// };
