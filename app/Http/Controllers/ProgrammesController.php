fe0e270231fd24904a3723ba8e2466d38a6ec769
Osuolale <Osuolale@Macs-MacBook-Pro.local>
2024-04-22 16:40:37 +0100

    Delete Ishiagu Poly Institution

--- app/Http/Controllers/ProgrammesController.php
+++ app/Http/Controllers/ProgrammesController.php
@@ -181,12 +181,12 @@ public function createInstitutionWithProgrammes(Request $request){
    
 // }
 
-public function deleteNuhuBamaliPolyInstitution(Request $request)
+public function deleteIshiaguPolyInstitution(Request $request)
 {
     // Validate the request if needed
 
     // Find the institution with the name "Afrihub Ict Institute, Abuja"
-    $institution = Institution::where('name', 'Nuhu Bamali Polytechnic, Zaria, Kaduna State')->first();
+    $institution = Institution::where('name', 'Federal College Of Forest Resources Management, Ishiagu')->first();
 
     if (!$institution) {
         return response()->json(['error' => 'Institution not found'], 404);
